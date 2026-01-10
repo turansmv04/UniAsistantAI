c0fdce11cf4a9c42c3141847d951518c16ace45d
turansmv004 <turansmv@gmail.com>
2026-01-11 03:48:34 +0400

    new

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ ESLint yoxlamasını deploy zamanı söndür
+  },
 };
 
 export default nextConfig;
